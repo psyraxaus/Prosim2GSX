8742e011524cdaba97fc3ade4f2da1b1435e6a92
Johncarlo <33170913+psyraxaus@users.noreply.github.com>
2025-05-22 08:55:20 +1000

    feat(UI): Implement Views foundation for PTT UI - Add Push-To-Talk (PTT) Views for new feature to be implemented

---
+++ Prosim2GSX/Views/Components/PttSettingsControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Prosim2GSX.Views.Components
+{
+    /// <summary>
+    /// Interaction logic for PttSettingsControl.xaml
+    /// </summary>
+    public partial class PttSettingsControl : UserControl
+    {
+        public PttSettingsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
