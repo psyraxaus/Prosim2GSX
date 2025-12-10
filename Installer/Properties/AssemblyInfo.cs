19cb3088f9b5d03f969666b7dfb33ca150437eb9
Fragtality <78614902+Fragtality@users.noreply.github.com>
2025-12-10 23:39:15 +0100

    CFIT/NuGet Updates / Set .NET 10.0.1 as Target

--- Installer/Properties/AssemblyInfo.cs
+++ Installer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Prosim2GSX Installer v0.5.3.0 (2025.12.08.1900)")]
+[assembly: AssemblyTitle("Prosim2GSX Installer v0.5.3.0 (2025.12.10.2213)")]
 [assembly: AssemblyDescription("Installer Application for Prosim2GSX")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fragtality")]
