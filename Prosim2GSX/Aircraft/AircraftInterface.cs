Fragtality/Fenix2GSX

Added Option if GPU should be connected on Startup when APU is running

Fenix2GSX/Aircraft/AircraftInterface.cs
@@ -51,6 +51,7 @@ namespace Fenix2GSX.Aircraft
         public virtual bool EnginesRunning => FenixInterface.GetEnginesRunning();
         public virtual bool IsFinalReceived => FenixInterface.GetFinalReceived();
         public virtual bool IsExternalPowerConnected => FenixInterface.GetExternalPowerConnected();
+        public virtual bool IsApuRunning => FenixInterface.IsApuRunning;
         public virtual bool IsApuBleedOn => FenixInterface.IsApuBleedOn;
         public virtual bool HasOpenDoors => FenixInterface.GetOpenDoors();
         public virtual bool IsBrakeSet => FenixInterface.GetBrake();

psyraxaus/Prosim2GSX

Prosim2GSX/Aircraft/AircraftInterface.cs
@@ -51,6 +51,7 @@ namespace Prosim2GSX.Aircraft
         public virtual bool EnginesRunning => ProsimInterface.GetEnginesRunning();
         public virtual bool IsFinalReceived => ProsimInterface.GetFinalReceived();
         public virtual bool IsExternalPowerConnected => ProsimInterface.GetExternalPowerConnected();
+        public virtual bool IsApuRunning => ProsimInterface.IsApuRunning;
         public virtual bool IsApuBleedOn => ProsimInterface.IsApuBleedOn;
         public virtual bool HasOpenDoors => ProsimInterface.GetOpenDoors();
         public virtual bool IsBrakeSet => ProsimInterface.GetBrake();