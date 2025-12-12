Fragtality/Fenix2GSX

Added Cleaning State to Monitor / Services in Bypassed State also considered completed (e.g. Cleaning) / Result of Service-Calls for Cleaning, Water & Lavatory now checked against Service State

Installer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Fenix2GSX Installer v0.5.3.0 (2025.11.25.1812)")]
+[assembly: AssemblyTitle("Fenix2GSX Installer v0.5.3.0 (2025.12.04.2313)")]
 [assembly: AssemblyDescription("Installer Application for Fenix2GSX")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fragtality")]

psyraxaus/Prosim2GSX

Installer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Prosim2GSX Installer v0.5.3.0 (2025.11.25.1812)")]
+[assembly: AssemblyTitle("Prosim2GSX Installer v0.5.3.0 (2025.12.04.2313)")]
 [assembly: AssemblyDescription("Installer Application for Prosim2GSX")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fragtality")]