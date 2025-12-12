Fragtality/Fenix2GSX

Option to place Fenix forward Stairs - when Walkaround is not skipped / Moved SkipWalkaround to Profile Settings / Set .NET 8.0.22 as Requirement / CFIT and Library Updates

Installer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Fenix2GSX Installer v0.5.3.0 (2025.11.07.1640)")]
+[assembly: AssemblyTitle("Fenix2GSX Installer v0.5.3.0 (2025.11.15.1340)")]
 [assembly: AssemblyDescription("Installer Application for Fenix2GSX")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fragtality")]

psyraxaus/Prosim2GSX

Installer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Prosim2GSX Installer v0.5.3.0 (2025.10.04.1642)")]
+[assembly: AssemblyTitle("Prosim2GSX Installer v0.5.3.0 (2025.11.15.1340)")]
 [assembly: AssemblyDescription("Installer Application for Prosim2GSX")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fragtality")]